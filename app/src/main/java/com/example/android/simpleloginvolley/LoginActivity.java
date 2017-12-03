c8d081da7025dd27c55a260f647feee14720d27e
Ahmed Hesham <eng.ahmed.86313@gmail.com>
2017-12-03 17:59:50 +0200

    Volley with JSONObject

--- app/src/main/java/com/example/android/simpleloginvolley/LoginActivity.java
+++ app/src/main/java/com/example/android/simpleloginvolley/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.simpleloginvolley;
 
+import android.app.DownloadManager;
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -20,14 +23,20 @@ import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private TextView userText;
+    private TextView geoText;
     private Button buttonLogin;
-    private String serverUrl = "http://192.168.1.5:8888/strawberry.png";
+    private String serverUrl = "http://dev.alsokary.com:8005/api/";
 
 
     @Override
@@ -35,32 +44,39 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        imageView = (ImageView) findViewById(R.id.img);
+        userText = (TextView) findViewById(R.id.tv_user);
+        geoText = (TextView) findViewById(R.id.tv_glocodata);
         buttonLogin = (Button) findViewById(R.id.bn);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ImageRequest imageRequest = new ImageRequest(serverUrl,
-                        new Response.Listener<Bitmap>() {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, serverUrl, null,
+                        new Response.Listener<JSONObject>() {
                             @Override
-                            public void onResponse(Bitmap response) {
-                                imageView.setImageBitmap(response);
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    userText.setText(response.getString("users"));
+                                    geoText.setText(response.getString("glocodata"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
-                        }, 0, 0, ImageView.ScaleType.CENTER_CROP, null,
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(getApplicationContext(),"Error...", Toast.LENGTH_SHORT).show();
-                                error.printStackTrace();
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error....", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                });
 
-                            }
-                        });
 
-                MySingleton.getInstance(getApplicationContext()).addToRequestQeue(imageRequest);
+                MySingleton.getInstance(getApplicationContext()).addToRequestQeue(jsonObjectRequest);
 
 
             }
         });
+
+
     }
 }
